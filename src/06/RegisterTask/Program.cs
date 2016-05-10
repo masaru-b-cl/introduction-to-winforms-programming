f4e283a8a0e6957ca5acc737e96578245f2ef918
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2016-05-10 10:40:58 +0900

    第6章 | サンプルコードを追加する

---
+++ src/06/RegisterTask/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace RegisterTask
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.ThreadException += (sender, e) =>
+            {
+                MessageBox.Show(
+                   "予期せぬエラーが発生しました。詳細はイベントログを確認して下さい。",
+                   "BMI計算機",
+                   MessageBoxButtons.OK,
+                   MessageBoxIcon.Stop);
+
+                EventLog.WriteEntry(
+                    "Application",
+                    e.Exception.ToString(),
+                    System.Diagnostics.EventLogEntryType.Error
+                    );
+
+                Application.Exit();
+            };
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
