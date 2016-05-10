c0409c2e8cdc158437b72e060910228946706c45
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2016-05-11 00:59:21 +0900

    第8章 | サンプルコード追加

---
+++ src/08/BrowseProduct/ProductForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrowseProduct
+{
+    public partial class ProductForm : Form
+    {
+        public ProductForm()
+        {
+            InitializeComponent();
+        }
+
+        public Product Product
+        {
+            set
+            {
+                productCodeTextBox.Text = value.Code;
+                productNameTextBox.Text = value.Name;
+                makerNameTextBox.Text = value.MakerName;
+            }
+        }
+    }
+}
